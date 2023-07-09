b20c785b382541ea8f6a8cb61ced5756163845a2
HugoAb2 <rodrigueshugoab2@gmail.com>
2023-07-09 19:34:35 -0300

    update: getting diseases code from dataset and showing their names as diagnostic

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "DocumentReceiver.h"
 #include "Disease.h"
 #include "DecisionTree.h"
+#include <algorithm>
 
 using namespace std;
 
@@ -19,8 +20,25 @@ int main() {
         dataset.push_back(currentD);
     }
 
+    //criando arvore, percorrendo e atualizando resultados em dataset
     DecisionTree tree;
     tree.buildTree(symptomsN);
     tree.runTree(dataset);
+
+    //verificando e printando resultados em dataset
+    if (dataset.empty()) cout << "No diseases match" << endl;
+    else{
+        vector<string> diagnostics;
+        for(Disease disease : dataset){
+            string answer = diseasesN[disease.getCode() - 1];
+            if (find(diagnostics.begin(), diagnostics.end(), answer) == diagnostics.end()) {
+                diagnostics.push_back(answer);
+            }
+        }
+        for(string diagnostic: diagnostics){
+            cout<< diagnostic << endl;
+        }
+    }
+
 
 }
